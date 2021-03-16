759dd53a6437b9ab48cf17b5408a04743983a2e9
Oleg V <oleg.vintonuk@gmail.com>
2021-03-16 09:17:40 +0200

    add js loops

--- 03-logical-operators/03-logicals-operators.js
+++ 03-logical-operators/03-logicals-operators.js
@@ -1,38 +1,37 @@
 'use strict';
 
-// logicals operators
+// Javascript logicals operators
 
-{
-	// || &&
+{ // || &&
 
 	// && return first false value otherwise return last true value
-	console.log('&& operator:', '0' && 1 && 0); 					// return 0 because 0 is first false Boolean value
-	console.log('&& operator:', 3 && 2 && true);					// return true because true is last true Boolean value
+	console.log('&& operator:', '0' && 1 && 0); 			// return 0 because 0 is first false Boolean value
+	console.log('&& operator:', 3 && 2 && true);			// return true because true is last true Boolean value
 
 	// || return first true value otherwise return last false value
-	console.log('|| operaor', 0 || 'hello' || NaN);				// return 'hello' because 'hello' is first true value
-	console.log('|| operaor', 0 || '' || NaN);						// return NaN because NaN is last false value
+	console.log('|| operaor', 0 || 'hello' || NaN);		// return 'hello' because 'hello' is first true value
+	console.log('|| operaor', 0 || '' || NaN);				// return NaN because NaN is last false value
 }
 
-{
-	// ! !!
+{ // ! !!
+
 	// convert values to Boolean (true, false) data type
-	console.log('!hello', !'hello'); 			// convert to Boolean false
-	console.log('!!hello', !!'hello'); 		// convert to Boolean true
+	console.log('!hello', !'hello'); 									// convert to Boolean false
+	console.log('!!hello', !!'hello'); 								// convert to Boolean true
 }
 
-{
-	// == === != !==
+{ // == === != !==
+
 	// compare data types and values
-	console.log('"1" == 1', '1' == 1);		// compare only values
-	console.log('"1" === 1', '1' === 1);	// compare types and values
+	console.log('"1" == 1', '1' == 1);								// compare only values
+	console.log('"1" === 1', '1' === 1);							// compare types and values
 }
 
-{
-	// < <= => >
+{ // < <= => >
+
 	// compare value A is greater or less or equal than value B
-	console.log('6 < 6', 6 < 6);					// false
-	console.log('6 <= 6', 6 <= 6);				// true
-	console.log('6 >= 6', 6 >= 6);				// true
-	console.log('6 > 6', 6 > 6);					// false
+	console.log('6 < 6', 6 < 6);											// false
+	console.log('6 <= 6', 6 <= 6);										// true
+	console.log('6 >= 6', 6 >= 6);										// true
+	console.log('6 > 6', 6 > 6);											// false
 }
