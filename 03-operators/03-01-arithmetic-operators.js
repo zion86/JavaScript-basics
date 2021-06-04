5305093bc6d55ca1683f11d4e77fe9912f811e4a
Oleg V <oleg.vintonuk@gmail.com>
2021-06-05 00:02:10 +0300

    update files

---
+++ 03-operators/03-01-arithmetic-operators.js
@@ -0,0 +1,37 @@
+'use strict';
+
+{
+  let digit = 5;
+
+  // addition
+  console.log('5 + 2: ', 5 + 2);            // 7
+
+  // subtraction
+  console.log('10 - 5: ', 10 - 5);          // 5
+
+  // multiplication
+  console.log('5 * 5: ', 5 * 5);            // 25
+
+  // division
+  console.log('25 / 5: ', 25 / 5);          // 5
+
+  // remainder
+  console.log('12 % 5: ', 12 % 5);          // 2
+
+  // post increment
+  digit++;                                  // 5
+  digit;                                    // 6
+
+  // post decrement
+  digit--;                                  // 6
+  digit;                                    // 5
+
+  // pre increment
+  ++digit;                                  // 6
+
+  // pre decrement
+  --digit;                                  // 5
+
+  // exponentiation 2 * 2 = 4 * 2 = 8
+  console.log('2 ** 3: ', 2 ** 3);          // 8
+}
