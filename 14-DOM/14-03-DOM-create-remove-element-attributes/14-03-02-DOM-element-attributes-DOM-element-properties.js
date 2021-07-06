6129068c611099c22b2924ce69262bac7ac563e2
Oleg V <oleg.vintonuk@gmail.com>
2021-07-06 14:46:03 +0300

    update files

--- 14-DOM/14-03-DOM-create-remove-element-attributes/14-03-02-DOM-element-attributes-DOM-element-properties.js
+++ 14-DOM/14-03-DOM-create-remove-element-attributes/14-03-02-DOM-element-attributes-DOM-element-properties.js
@@ -2,35 +2,43 @@
 
 const btn = document.querySelector('#submit-btn');
 
-{ // DOM object element properties
+{ // DOM NODE is a simple Object. DOM object has element properties
 
   // create new property in <button> element
   btn.btnName = {
     text: 'Submit form',
   }
 
-  // create function in <button> element
+  // create method in <button> element
   btn.showBtnName = function () {
     console.log(this.btnName);
   }
   btn.showBtnName();
 
+  // read standart HTML attribute
+  btn.tagName;                                    // 'BUTTON'
+
   // update <button> textContent
   btn.textContent = btn.btnName.text;
 }
+
+
+{ // DOM object element attributes: standart, custom
+
+  // attributes always is: 'String data type'
+  btn.setAttribute('custom-attr', 'some value');  // create new attribute in DOM object element
+  btn.hasAttribute('secured');                    // returns true or false, check if DOM object element attribute exists
+  btn.getAttribute('secured');                    // get attribute value from DOM object element
+  btn.removeAttribute('data-user-info');          // remove attribute from htmlElem
+  btn.attributes;                                 // returns NamedNodeMap collection with list data attributes { name: value }
+  // NamedNodeMap {0: type, 1: id, 2: style, 3: secured, 4: data-order-state, 5: custom-attr, type: type, id: id, style: style, secured: secured, data-order-state: data-order-state, …}
+}
+
 
 { // DOM object element dataset attributes, non-standart attributes === user attributer
 
   btn.dataset.orderState = 'pendind';             // create new data-* attribute 'pending'
   btn.dataset.userInfo = "30 years";              // change existing data-* attributes
-  btn.dataset;                                    // return DOMStringMap Object with data- attributes { name: value }
-}
-
-{ // DOM object element attributes: standart, custom
-
-  btn.setAttribute('custom-attr', 'some value');  // create new attribute in DOM object element
-  btn.hasAttribute('secured');                    // return true or false, check if DOM object element attribute exists
-  btn.getAttribute('secured');                    // get attribute value from DOM object element
-  btn.removeAttribute('data-user-info');          // remove attribute from htmlElem
-  btn.attributes;                                 // return NamedNodeMap collection with list data attributes { name: value }
+  btn.dataset;                                    // returns DOMStringMap Object with data-attributes { name: value }
+  // DOMStringMap {userInfo: "30 years", orderState: "pendind"}
 }
